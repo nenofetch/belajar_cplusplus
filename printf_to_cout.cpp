db350df2e47f8c6dd946c82b46db9cd8bbc34ac0
Neno Arisma <nenoarisma@gmail.com>
2021-11-06 22:01:17 +0700

    Update

---
+++ printf_to_cout.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int nilai = 18;
+	char huruf = 'R';
+	cout << "Huruf abjad yang akan di cek adalah = " << huruf << endl;
+	cout << "Bila huruf tersbut adalah " << huruf << " maka urutan abjadnya adalah " << nilai << endl;
+	return 0;
+}
+
